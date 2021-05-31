81f975f000aa77a434183373c2805c9fcd5c5302
Proske, Nicloas <n.proske@oth-aw.de>
2021-05-31 22:28:26 +0200

    Update UI
    - Reformat code (npx prettier --write .)
    - Switch locale to "de"
    - Add reset css files
    - Add FontAwesome
    
    
    Former-commit-id: 432d4f8fa20e7916a1b168f45c22f24797978414

--- sys-src/backend/filestore.ts
+++ sys-src/backend/filestore.ts
@@ -1,45 +1,51 @@
 import fs from "fs";
 
-const filesFolder = './data';
+const filesFolder = "./data";
 const cache = new Map<string, any>();
 
-export default function getFromCache<T>(path: string, fetchDataCallback: () => Promise<string>, transformData: (text: string) => Promise<T>): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-        if (cache && cache.has(path)) {
-            resolve(cache.get(path));
+export default function getFromCache<T>(
+  path: string,
+  fetchDataCallback: () => Promise<string>,
+  transformData: (text: string) => Promise<T>
+): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    if (cache && cache.has(path)) {
+      resolve(cache.get(path));
+    } else {
+      const filePath = `${filesFolder}/${path}`;
+      fs.mkdir(
+        filesFolder,
+        {
+          recursive: true,
+        },
+        (err) => {
+          if (err) reject(err);
         }
-        else {
-            const filePath = `${filesFolder}/${path}`;
-            fs.mkdir(filesFolder, {
-                recursive: true,
-            }, (err) => {
-                if (err) reject(err);
-            });
-            fs.readFile(filePath, (err, data) => {
-                if (err) {
-                    fetchDataCallback()
-                        .then(data => {
-                            fs.writeFile(filePath, data, (err) => {
-                                reject(err);
-                            });
-                            transformData(data)
-                                .then(transformed => {
-                                    cache.set(path, transformed);
-                                    resolve(transformed);
-                                })
-                                .catch(reject);
-                        })
-                        .catch(reject);
-                }
-                else {
-                    transformData(data.toString())
-                        .then(transformed => {
-                            cache.set(path, transformed);
-                            resolve(transformed);
-                        })
-                        .catch(reject);
-                }
-            });
+      );
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          fetchDataCallback()
+            .then((data) => {
+              fs.writeFile(filePath, data, (err) => {
+                reject(err);
+              });
+              transformData(data)
+                .then((transformed) => {
+                  cache.set(path, transformed);
+                  resolve(transformed);
+                })
+                .catch(reject);
+            })
+            .catch(reject);
+        } else {
+          transformData(data.toString())
+            .then((transformed) => {
+              cache.set(path, transformed);
+              resolve(transformed);
+            })
+            .catch(reject);
         }
-    });
+      });
+    }
+  });
 }
--- sys-src/covid-dashboard/src/app/components/home/home.component.spec.ts
+++ sys-src/covid-dashboard/src/app/components/home/home.component.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapComponent } from './map.component';
+import { HomeComponent } from './home.component';
 
-describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MapComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
