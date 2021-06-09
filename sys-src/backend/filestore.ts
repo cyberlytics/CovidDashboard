0126835530ff24650a8db8bb70efa0716ac9433a
Bauer, Tobias <t.bauer@oth-aw.de>
2021-06-09 17:30:02 +0200

    - Add versioning of published and aggregated data - Automatic detection if today is a new day and RKI probably published some new stuff - Automatic cache clear if new data should be loaded (every day at 04:00)
    Former-commit-id: 354f8d21c5cdb7c10e34b575fd10342b7e9d4ca4

--- sys-src/backend/filestore.ts
+++ sys-src/backend/filestore.ts
@@ -1,6 +1,11 @@
 import fs from "fs";
+import sevenZip from "7zip-min";
+import { dateToString } from "./util";
 
 const filesFolder = "./data";
+const archiveFolder = "./archive";
+const filesTempFolder = "./data_temp";
+const lastUpdateFileName = filesFolder + '/last_update.txt';
 const cache = new Map<string, any>();
 
 export default function getFromCache<T>(
@@ -9,43 +14,103 @@ export default function getFromCache<T>(
   transformData: (text: string) => Promise<T>
 ): Promise<T> {
   return new Promise<T>((resolve, reject) => {
-    if (cache && cache.has(path)) {
-      resolve(cache.get(path));
-    } else {
-      const filePath = `${filesFolder}/${path}`;
-      fs.mkdir(
-        filesFolder,
-        {
-          recursive: true,
-        },
-        (err) => {
-          if (err) reject(err);
-        }
-      );
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          fetchDataCallback()
-            .then((data) => {
-              fs.writeFile(filePath, data, (err) => {
+    ensureFolder(archiveFolder, reject);
+    ensureFolder(filesFolder, reject);
+
+    const archiveDate = new Date(Date.parse(dateToString(new Date())));
+
+    fs.readFile(lastUpdateFileName, (err, data) => {
+      if (err) {
+        fs.writeFile(lastUpdateFileName, dateToString(archiveDate), (err) => {
+          if (err) {
+            reject(err);
+          }
+        });
+        handleLoading();
+      }
+      else {
+        const lastArchiveDate = new Date(Date.parse(data.toString()));
+        if (lastArchiveDate < archiveDate && new Date().getHours() > 4) { // Make sure RKI has enough time to publish new data
+          cache.clear();
+          fs.rm(filesTempFolder, {
+            force: true,
+            recursive: true,
+          }, (err) => {
+            if (err) {
+              reject(err);
+            }
+            fs.rename(filesFolder, filesTempFolder, (err) => {
+              ensureFolder(filesFolder, reject);
+              if (err) {
                 reject(err);
-              });
-              transformData(data)
-                .then((transformed) => {
-                  cache.set(path, transformed);
-                  resolve(transformed);
-                })
-                .catch(reject);
-            })
-            .catch(reject);
-        } else {
-          transformData(data.toString())
-            .then((transformed) => {
-              cache.set(path, transformed);
-              resolve(transformed);
-            })
-            .catch(reject);
+              }
+              sevenZip.pack(filesTempFolder,
+                `${archiveFolder}/data_${dateToString(archiveDate)}.7z`,
+                err => {
+                  if (err) {
+                    reject(err);
+                  }
+                  fs.writeFile(lastUpdateFileName, dateToString(archiveDate), (err) => {
+                    if (err) {
+                      reject(err);
+                    }
+                  });
+                });
+              handleLoading();
+            });
+          });
         }
-      });
+        else {
+          handleLoading();
+        }
+      }
+    });
+
+    function handleLoading() {
+      if (cache && cache.has(path)) {
+        resolve(cache.get(path));
+      } else {
+        const filePath = `${filesFolder}/${path}`;
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            fetchDataCallback()
+              .then((data) => {
+                fs.writeFile(filePath, data, (err) => {
+                  reject(err);
+                });
+                transformData(data)
+                  .then((transformed) => {
+                    cache.set(path, transformed);
+                    resolve(transformed);
+                  })
+                  .catch(reject);
+              })
+              .catch(reject);
+          } else {
+            transformData(data.toString())
+              .then((transformed) => {
+                cache.set(path, transformed);
+                resolve(transformed);
+              })
+              .catch(reject);
+          }
+        });
+      }
     }
+
   });
+}
+
+function ensureFolder(path: string, reject: (a: any) => void): void {
+  fs.mkdir(
+    path,
+    {
+      recursive: true,
+    },
+    (err) => {
+      if (err) {
+        reject(err);
+      }
+    }
+  );
 }
