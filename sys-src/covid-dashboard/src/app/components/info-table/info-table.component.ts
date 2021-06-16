83cca5c3fc2a3613530a05cb0ab546988ebb6b67
Lukas Kleinlein <l.kleinlein@oth-aw.de>
2021-06-16 18:30:22 +0200

    added data to list

--- sys-src/covid-dashboard/src/app/components/info-table/info-table.component.ts
+++ sys-src/covid-dashboard/src/app/components/info-table/info-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { County } from 'src/app/services/county';
+import { NetworkService } from 'src/app/services/network/network.service';
 
 @Component({
   selector: 'app-info-table',
@@ -8,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class InfoTableComponent implements OnInit {
   selectedBookmark: boolean = false;
 
-  constructor() {}
+  public allCountys = [] as County[];
+
+  constructor(
+    private network: NetworkService
+  ) {
+    this.network.getAllCountyIncidences().subscribe((res) => {
+      console.log('res in table', res);
+      this.allCountys = res;
+    })
+  }
 
   ngOnInit(): void {}
 }
