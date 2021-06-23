838434d6b3a53dc06f7eabd7e1655faa2c59b6ea
Hahn, Albert <a.hahn@oth-aw.de>
2021-06-23 20:18:27 +0200

    added table for states with backend data
    Former-commit-id: 0e89048488d8df6053b4e923bde1866cd6adf10a

--- sys-src/covid-dashboard/src/app/services/alltypes.ts
+++ sys-src/covid-dashboard/src/app/services/alltypes.ts
@@ -69,3 +69,48 @@ export type AreaData = {
   name: string;
   series: ScaleData[];
 };
+
+export type VaccineDiff = {
+  0: number;
+  1: VaccineDiffDetails;
+}
+
+export type VaccineDiffDetails = {
+  DeltaProportionFirstVaccinations: number;
+  DeltaProportionSecondVaccinations: number;
+  DeltaSumAstraZeneca: number;
+  DeltaSumBioNTech: number;
+  DeltaSumFirstAstraZeneca: number;
+  DeltaSumFirstBioNTech: number;
+  DeltaSumFirstModerna: number;
+  DeltaSumFirstVaccinations: number;
+  DeltaSumJohnsonAndJohnson: number;
+  DeltaSumModerna: number;
+  DeltaSumSecondAstraZeneca: number;
+  DeltaSumSecondBioNTech: number;
+  DeltaSumSecondModerna: number;
+  DeltaSumSecondVaccinations: number;
+  DeltaSumVaccinations: number;
+}
+
+export type VaccineCombined = {
+  Date: string;
+  ProportionFirstVaccinations: number;
+  ProportionSecondVaccinations: number;
+  StateId: number;
+  StateName: string;
+  SumAstraZeneca: number;
+  SumBioNTech: number;
+  SumFirstAstraZeneca: number;
+  SumFirstBioNTech: number;
+  SumFirstModerna: number;
+  SumFirstVaccinations: number;
+  SumJohnsonAndJohnson: number;
+  SumModerna: number;
+  SumSecondAstraZeneca: number;
+  SumSecondBioNTech: number;
+  SumSecondModerna: number;
+  SumSecondVaccinations: number;
+  SumVaccinations: number;
+  diff: VaccineDiffDetails;
+}
