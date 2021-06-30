f84e85532e8e898895c58472b50129026be81a22
Nicolas Proske <nico.proske@hotmail.de>
2021-06-30 21:07:54 +0200

    Improve tests for FavoriteService
    - Add branch testing

--- sys-src/frontend/src/app/services/favorites/favorites.service.spec.ts
+++ sys-src/frontend/src/app/services/favorites/favorites.service.spec.ts
@@ -1,12 +1,15 @@
-import {TestBed} from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 
 import {FavoritesService} from './favorites.service';
+import {ClipboardService} from "ngx-clipboard";
 
 describe('FavoritesService', () => {
   let service: FavoritesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [ClipboardService]
+    });
     service = TestBed.inject(FavoritesService);
   });
 
@@ -40,6 +43,36 @@ describe('FavoritesService', () => {
     expect(service.favorites.includes(1)).toBeTrue();
   })
 
+  it('should call removeFavorite if isFavorite returns true', () => {
+    const spy = spyOn(service, 'removeFavorite');
+
+    service.favorites = [1]
+    service.toggleFavorite(1);
+
+    // Check if removeFavorite was called
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call addFavorite if isFavorite returns false', () => {
+    const spy = spyOn(service, 'addFavorite');
+
+    service.favorites = []
+    service.toggleFavorite(1);
+
+    // Check if addFavorite was called
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call syncLocalStorage', () => {
+    const spy = spyOn(service, 'syncLocalStorage');
+
+    service.favorites = [1]
+    service.replaceFavorites([2]);
+
+    // Check if syncLocalStorage was called
+    expect(spy).toHaveBeenCalled();
+  })
+
   it('should contain the correct share link', () => {
     service.favorites = [1, 2, 3]
 
@@ -55,12 +88,32 @@ describe('FavoritesService', () => {
     expect(service.favorites.includes(1)).toBeTrue();
   })
 
+  it('should call syncLocalStorage', () => {
+    const spy = spyOn(service, 'syncLocalStorage');
+
+    service.favorites = []
+    service.addFavorite(1);
+
+    // Check if syncLocalStorage was called
+    expect(spy).toHaveBeenCalled();
+  })
+
   it('should remove id from array', () => {
     service.favorites = [1]
     service.removeFavorite(1);
 
     // Check if array not includes 1
     expect(service.favorites.includes(1)).toBeFalse();
+  })
+
+  it('should call syncLocalStorage', () => {
+    const spy = spyOn(service, 'syncLocalStorage');
+
+    service.favorites = [1]
+    service.removeFavorite(1);
+
+    // Check if syncLocalStorage was called
+    expect(spy).toHaveBeenCalled();
   })
 
   it('should remove duplicate ids from array', () => {
@@ -80,4 +133,13 @@ describe('FavoritesService', () => {
       return i === 0 || x >= testNumberArray[i - 1];
     })).toBeTrue();
   })
+
+  it('should call localStorage.setItem', fakeAsync(() => {
+    const spy = spyOn(localStorage, 'setItem');
+
+    service.syncLocalStorage();
+
+    // Check if localStorage.setItem was called
+    expect(spy).toHaveBeenCalled();
+  }))
 });
