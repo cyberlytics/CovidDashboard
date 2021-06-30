6090102fb93589070ace82fbcb8c81c9ae132187
Proske, Nicloas <n.proske@oth-aw.de>
2021-06-30 20:27:49 +0200

    Add tests to NavBarComponent
    - Add documentation to code
    
    Former-commit-id: 1944b8ebc0525b93cb9e2a4367dfc50c4c6feab7

--- sys-src/frontend/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ sys-src/frontend/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,7 +1,9 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {NavBarComponent} from './nav-bar.component';
 import {RouterTestingModule} from "@angular/router/testing";
+
+const defaultShareButtonText: string = "Favoriten teilen"
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -24,4 +26,22 @@ describe('NavBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have shareButtonText', () => {
+    // Check if shareButtonText has correct default text
+    expect(component.shareButtonText).toBe(defaultShareButtonText);
+  });
+
+  it('should change shareButtonText', fakeAsync(() => {
+    component.generateShareLink();
+
+    // Check if shareButtonText changed to new value
+    expect(component.shareButtonText).toBe("Kopiert");
+
+    // Wait 1500ms
+    tick(1500);
+
+    // Check if shareButtonText changed back to default text
+    expect(component.shareButtonText).toBe(defaultShareButtonText);
+  }));
 });
