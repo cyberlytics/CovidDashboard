017cb89bc4582e487e2f67406552c923070a7995
Nicolas Proske <nico.proske@hotmail.de>
2021-06-30 20:04:33 +0200

    Add tests for FavoriteService
    - Add documentation to code

--- sys-src/frontend/src/app/services/favorites/favorites.service.spec.ts
+++ sys-src/frontend/src/app/services/favorites/favorites.service.spec.ts
@@ -13,4 +13,71 @@ describe('FavoritesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return if id is in array', () => {
+    service.favorites = [1]
+
+    // Check if function return true when array includes id
+    expect(service.isFavorite(1)).toBeTrue();
+
+    // Check if function return false when array not includes id
+    expect(service.isFavorite(2)).toBeFalse();
+  })
+
+  it('should remove favorite if id is in array', () => {
+    service.favorites = [1]
+    service.toggleFavorite(1);
+
+    // Check if array not includes 1
+    expect(service.favorites.includes(1)).toBeFalse();
+  })
+
+  it('should add favorite if id is not in array', () => {
+    service.favorites = []
+    service.toggleFavorite(1);
+
+    // Check if array includes 1
+    expect(service.favorites.includes(1)).toBeTrue();
+  })
+
+  it('should contain the correct share link', () => {
+    service.favorites = [1, 2, 3]
+
+    // Check for correct link
+    expect(service.generateShareLink() === ('https://covidash.de/share/1,2,3')).toBeTrue();
+  })
+
+  it('should add id to array', () => {
+    service.favorites = []
+    service.addFavorite(1);
+
+    // Check if array includes 1
+    expect(service.favorites.includes(1)).toBeTrue();
+  })
+
+  it('should remove id from array', () => {
+    service.favorites = [1]
+    service.removeFavorite(1);
+
+    // Check if array not includes 1
+    expect(service.favorites.includes(1)).toBeFalse();
+  })
+
+  it('should remove duplicate ids from array', () => {
+    service.favorites = [3, 1, 2, 2]
+    const testNumberArray = service.cleanUpArray() as number[];
+
+    // Check if array includes 1, 2 and 3
+    expect(testNumberArray.includes(1)).toBeTrue();
+    expect(testNumberArray.includes(2)).toBeTrue();
+    expect(testNumberArray.includes(3)).toBeTrue();
+
+    // Check if duplicates where removed
+    expect(testNumberArray.length).toBe(3);
+
+    // Check if array is sorted ascending
+    expect(testNumberArray.every((x, i) => {
+      return i === 0 || x >= testNumberArray[i - 1];
+    })).toBeTrue();
+  })
 });
--- sys-src/frontend/src/app/services/favorites/favorites.service.ts
+++ sys-src/frontend/src/app/services/favorites/favorites.service.ts
@@ -7,7 +7,7 @@ import {ClipboardService} from "ngx-clipboard";
 })
 export class FavoritesService {
   private readonly key: string = 'favorites';
-  private readonly favorites = [] as number[];
+  public favorites = [] as number[];
 
   constructor(private clipboardService: ClipboardService) {
     const rawData = localStorage.getItem(this.key);
@@ -17,43 +17,76 @@ export class FavoritesService {
     }
   }
 
+  /**
+   * Add id to favorites if id is not favorite, else remove id from favorites
+   * @param id
+   */
   public toggleFavorite(id: number): void {
     this.isFavorite(id) ? this.removeFavorite(id) : this.addFavorite(id);
   }
 
+  /**
+   * Clear favorites and add new ids to favorites
+   * @param ids
+   */
   public replaceFavorites(ids: number[]): void {
     this.cleanUpArray(ids).forEach((id) => this.addFavorite(id));
     this.syncLocalStorage();
   }
 
-  public generateShareLink(): void {
+  /**
+   * Generate a share link
+   */
+  public generateShareLink(): string {
     let shareLink = 'https://covidash.de/share/';
     this.favorites.forEach((id, index) => {
       shareLink += id + (index === this.favorites.length - 1 ? '' : ',');
     });
     this.clipboardService.copy(shareLink);
+    return shareLink;
   }
 
+  /**
+   * Check if id in favorite or not
+   * @param id
+   * @returns true if id is favorite, else instead
+   */
   public isFavorite(id: number): boolean {
     return this.favorites.includes(id);
   }
 
-  private addFavorite(id: number): void {
+  /**
+   * Add new id to favorites
+   * @param id
+   */
+  public addFavorite(id: number): void {
     this.favorites.push(id);
     this.syncLocalStorage();
   }
 
-  private removeFavorite(id: number): void {
+  /**
+   * Remove id from favorites
+   * @param id
+   */
+  public removeFavorite(id: number): void {
     const index: number = this.favorites.indexOf(id);
     this.favorites.splice(index, 1);
     this.syncLocalStorage();
   }
 
-  private syncLocalStorage(): void {
-    localStorage.setItem(this.key, JSON.stringify(this.cleanUpArray()));
+  /**
+   * Remove duplicates and sort ids in favorite array ascending
+   * @param array
+   */
+  public cleanUpArray(array: number[] = this.favorites): number[] {
+    return Array.from(new Set(array)).sort((a, b) => a - b);
   }
 
-  private cleanUpArray(array: number[] = this.favorites): number[] {
-    return Array.from(new Set(array)).sort((a, b) => a - b);
+  /**
+   * Clean up favorite array, stringify and update LocalStorage
+   * @private
+   */
+  private syncLocalStorage(): void {
+    localStorage.setItem(this.key, JSON.stringify(this.cleanUpArray()));
   }
 }
