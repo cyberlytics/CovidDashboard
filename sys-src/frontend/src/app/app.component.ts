9eab9eab1555f73390e99cbaf8773e0204b6e9b2
Lukas Kleinlein <l.kleinlein@oth-aw.de>
2021-08-06 10:57:03 +0200

    first changes

--- sys-src/frontend/src/app/app.component.ts
+++ sys-src/frontend/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import {Component} from '@angular/core';
+import type { OnInit} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { ResizeService } from './services/resize/resize.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private resize: ResizeService) {
+    console.log(window.innerWidth);
+  }
+
+  ngOnInit() {
+    console.log('oninit')
+    this.checkScreen();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreen();
+  }
+
+  private checkScreen() {
+    this.resize.isMobile = (window.innerWidth < 900);
+    // console.log(window.innerWidth);
+  }
 }
