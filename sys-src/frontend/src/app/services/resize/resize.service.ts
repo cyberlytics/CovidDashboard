dd671d7d94eafc84a31f26ebbdc16f8efc432d9e
Lukas Kleinlein <l.kleinlein@oth-aw.de>
2021-08-07 18:58:42 +0200

    navbar setting saved in localstorage

--- sys-src/frontend/src/app/services/resize/resize.service.ts
+++ sys-src/frontend/src/app/services/resize/resize.service.ts
@@ -5,9 +5,28 @@ import { Injectable } from '@angular/core';
 })
 export class ResizeService {
     // only used for deciding if view is mobile or desktop
-    public isMobile = false;
-    public navBarIsShown = true;
+    private _isMobile = false;
+    private _navBarIsShown = true;
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() { }
+
+    public get isMobile (): boolean {
+      return this._isMobile
+    }
+
+    public set isMobile(value: boolean) {
+      this._isMobile = value;
+    }
+
+    public get navBarIsShown(): boolean {
+      return this._navBarIsShown;
+    }
+
+    public set navBarIsShown(value: boolean) {
+      this._navBarIsShown = value;
+      localStorage.setItem('navBar', this._navBarIsShown.toString());
+    }
+
+
 }
