0b50d73eacbe3094d7a5d3146c6b83c18bff0786
Lukas Kleinlein <l.kleinlein@oth-aw.de>
2021-08-16 17:23:37 +0200

    navBar vaccines fix

--- sys-src/frontend/src/app/pages/vaccinations/vaccinations.component.ts
+++ sys-src/frontend/src/app/pages/vaccinations/vaccinations.component.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import type {OnInit} from '@angular/core';
 import {Component} from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { ResizeService } from 'src/app/services/resize/resize.service';
 
 @Component({
   selector: 'app-vaccinations',
@@ -8,7 +10,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./vaccinations.component.scss'],
 })
 export class VaccinationsComponent implements OnInit {
-  constructor() {
+  constructor(
+    public resize: ResizeService
+  ) {
   }
 
   ngOnInit(): void {
