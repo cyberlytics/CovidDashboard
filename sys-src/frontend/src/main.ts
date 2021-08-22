3907a30115a8c6223035ad634035f634e5125c89
Lukas Kleinlein <l.kleinlein@oth-aw.de>
2021-08-22 20:43:29 +0200

    override console.log

--- sys-src/frontend/src/main.ts
+++ sys-src/frontend/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
@@ -5,6 +6,7 @@ import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 import 'hammerjs';
 
+console.log = function(){};
 if (environment.production) {
   enableProdMode();
 }
